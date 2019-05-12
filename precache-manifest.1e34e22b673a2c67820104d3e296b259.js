d0d43e0694be7893461b9e89404b917bff9ae0a0
liminjun <v-leeli@hotmail.com>
2019-05-12 21:20:48 +0800

    feat:update the resume page.

---
+++ precache-manifest.1e34e22b673a2c67820104d3e296b259.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "5a0caee4096ed5451319",
+    "url": "/static/css/main.2c29ae6b.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a05d7b23dabaa3e85d14",
+    "url": "/static/js/2.a05d7b23.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "5a0caee4096ed5451319",
+    "url": "/static/js/main.5a0caee4.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "a05d7b23dabaa3e85d14",
+    "url": "/static/css/2.15cde2bc.chunk.css"
+  },
+  {
+    "revision": "956791606cbdc952524ec1cc0fa94a8c",
+    "url": "/index.html"
+  }
+];
